3f7622ff3185992b7b47b319567265de8da3e92b
Dineo <2816258@students.wits.ac.za>
2026-07-10 13:36:40 +0200

    My first program.Add hello.sh - basic bash variables and commands

---
+++ beginner/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME="Dineo"
+DATE=$(date +%Y-%m-%d)
+TIME=$(date +%H:%M)
+
+echo "HELLO $NAME"
+echo "Today is $DATE"
+echo "Current time is $TIME"
+echo "You are currently logged in as: $USER"
+echo "You are currently in: $PWD"
